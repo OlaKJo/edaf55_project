fd86a16ec3f392a9db83ffaed525b4adc3aa14b5
Erman <Erman@192.168.0.140>
2017-11-15 17:49:21 +0100

    Implemented DisplayMonitor, removed obsolete GUI class

--- edaf55_project_java/src/Client/DisplayMonitor.java
+++ edaf55_project_java/src/Client/DisplayMonitor.java
@@ -3,24 +3,46 @@ package Client;
 public class DisplayMonitor {
 
 	private int[] pic_1, pic_2;
-	
-	public synchronized void updatePicture1(int[] pic1) {
-		
+	// flags for pics received and available to fetch
+	private boolean pic1Available, pic2Available;
+
+	public DisplayMonitor() {
+		pic1Available = false;
+		pic2Available = false;
 	}
-	
-	public synchronized void updatePicture2(int[] pic2) {
-		
-		
+
+	public synchronized void updatePicture1(int[] pic) {
+		this.pic_1 = pic;
+		pic1Available = true;
+		notifyAll();
 	}
-	
+
+	public synchronized void updatePicture2(int[] pic) {
+		this.pic_2 = pic;
+		pic2Available = true;
+		notifyAll();
+	}
+
 	public synchronized int[] getPic1() {
-		return null;
-		
+
+		try {
+			while (!pic1Available) wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		pic1Available = false;
+		return pic_1;
 	}
-	
+
 	public synchronized int[] getPic2() {
-		return null;
-		
+
+		try {
+			while (!pic2Available) wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		pic2Available = false;
+		return pic_2;
 	}
-	
+
 }
