1a351c19c4666b86819cf084ceaae2c5ca48947b
Hannes <Hannes@Hannes-Laptop>
2017-11-22 10:46:05 +0100

    made a central main

--- edaf55_project_java/src/client/CameraClientMain.java
+++ edaf55_project_java/src/client/CameraClientMain.java
@@ -1,11 +1,14 @@
 package client;
 
+import networking.*;
+
 public class CameraClientMain {
 
 	//Manage client side classes
 	public static void main(String[] args) {
 		ClientMonitor cm = new ClientMonitor();
-		
+		ModeNetStarter mn = new ModeNetStarter(cm, "127.0.0.1", 8000);
+		mn.startMe();
 	}
 	
 }
