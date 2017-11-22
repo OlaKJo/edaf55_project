cfb59ddec596e34e0387319f6532af0606e8f722
OlaJohansson <ola.oj.johansson@gmail.com>
2017-11-22 12:48:30 +0100

    refatored camera_monitor to separate file and created new header. Updated Makefile.fake to include these new files in compilation

--- C/source/experimental/server.c
+++ C/source/experimental/server.c
@@ -1,4 +1,6 @@
 #include "server_common.h"
+#include "camera_monitor.h"
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -11,8 +13,6 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define MOVIE_MODE 1
-#define IDLE_MODE 0
 #define USE_CAMERA
 
 // some diagnostic printouts
@@ -32,7 +32,6 @@
 #define MOTION_PORT 9090
 #endif
 
-#define BUFSIZE 50000
 struct client{
     int  connfd;
     byte sendBuff[BUFSIZE];
@@ -44,68 +43,68 @@ struct client{
 
 // MONITOR STUFF!--------------------------------------------------------------
 
-struct camera_monitor {
-  byte pictureData[BUFSIZE];
-  int mode;
-  bool pic_taken;
-  bool pic_sent;
-};
-
-struct camera_monitor * cam_mon;
-pthread_mutex_t camera_mutex;
-
-void set_mode(int val) {
-  pthread_mutex_lock(&camera_mutex);
-  cam_mon->mode = val;
-  pthread_mutex_unlock(&camera_mutex);
-}
-
-bool set_pic_taken(bool val) {
-  pthread_mutex_lock(&camera_mutex);
-  cam_mon->pic_taken = val;
-  pthread_mutex_unlock(&camera_mutex);
-  return cam_mon->pic_taken;
-}
-
-bool set_pic_sent(bool val) {
-  pthread_mutex_lock(&camera_mutex);
-  cam_mon->pic_sent = val;
-  pthread_mutex_unlock(&camera_mutex);
-  return cam_mon->pic_sent;
-}
-
-void get_pic(byte * pic_copy) {
-  pthread_mutex_lock(&camera_mutex);
-  memcpy(pic_copy, cam_mon->pictureData, BUFSIZE);
-  pthread_mutex_unlock(&camera_mutex);
-}
-
-void save_pic(byte * pic) {
-  pthread_mutex_lock(&camera_mutex);
-  memcpy(cam_mon->pictureData, pic, BUFSIZE);
-  pthread_mutex_unlock(&camera_mutex);
-}
-
-int get_mode(void) {
-  pthread_mutex_lock(&camera_mutex);
-  int current_mode = cam_mon->mode;
-  pthread_mutex_unlock(&camera_mutex);
-  return current_mode;
-}
-
-bool get_pic_taken(void) {
-  pthread_mutex_lock(&camera_mutex);
-  bool current_pic_taken = cam_mon->pic_taken;
-  pthread_mutex_unlock(&camera_mutex);
-  return current_pic_taken;
-}
-
-bool get_pic_sent(void) {
-  pthread_mutex_lock(&camera_mutex);
-  bool current_pic_sent = cam_mon->pic_sent;
-  pthread_mutex_unlock(&camera_mutex);
-  return current_pic_sent;
-}
+// struct camera_monitor {
+//   byte pictureData[BUFSIZE];
+//   int mode;
+//   bool pic_taken;
+//   bool pic_sent;
+// };
+//
+// struct camera_monitor * cam_mon;
+// pthread_mutex_t camera_mutex;
+//
+// void set_mode(int val) {
+//   pthread_mutex_lock(&camera_mutex);
+//   cam_mon->mode = val;
+//   pthread_mutex_unlock(&camera_mutex);
+// }
+//
+// bool set_pic_taken(bool val) {
+//   pthread_mutex_lock(&camera_mutex);
+//   cam_mon->pic_taken = val;
+//   pthread_mutex_unlock(&camera_mutex);
+//   return cam_mon->pic_taken;
+// }
+//
+// bool set_pic_sent(bool val) {
+//   pthread_mutex_lock(&camera_mutex);
+//   cam_mon->pic_sent = val;
+//   pthread_mutex_unlock(&camera_mutex);
+//   return cam_mon->pic_sent;
+// }
+//
+// void get_pic(byte * pic_copy) {
+//   pthread_mutex_lock(&camera_mutex);
+//   memcpy(pic_copy, cam_mon->pictureData, BUFSIZE);
+//   pthread_mutex_unlock(&camera_mutex);
+// }
+//
+// void save_pic(byte * pic) {
+//   pthread_mutex_lock(&camera_mutex);
+//   memcpy(cam_mon->pictureData, pic, BUFSIZE);
+//   pthread_mutex_unlock(&camera_mutex);
+// }
+//
+// int get_mode(void) {
+//   pthread_mutex_lock(&camera_mutex);
+//   int current_mode = cam_mon->mode;
+//   pthread_mutex_unlock(&camera_mutex);
+//   return current_mode;
+// }
+//
+// bool get_pic_taken(void) {
+//   pthread_mutex_lock(&camera_mutex);
+//   bool current_pic_taken = cam_mon->pic_taken;
+//   pthread_mutex_unlock(&camera_mutex);
+//   return current_pic_taken;
+// }
+//
+// bool get_pic_sent(void) {
+//   pthread_mutex_lock(&camera_mutex);
+//   bool current_pic_sent = cam_mon->pic_sent;
+//   pthread_mutex_unlock(&camera_mutex);
+//   return current_pic_sent;
+// }
 
 // END MONITOR STUFF!----------------------------------------------------------
 
@@ -450,10 +449,12 @@ static void client_init(struct client* client)
 }
 int serve_clients(struct global_state* state)
 {
-    cam_mon = malloc(sizeof(*cam_mon));
-    cam_mon->pic_sent = false;
-    cam_mon->pic_taken = false;
-    cam_mon->mode = IDLE_MODE;
+    // cam_mon = malloc(sizeof(*cam_mon));
+    // cam_mon->pic_sent = false;
+    // cam_mon->pic_taken = false;
+    // cam_mon->mode = IDLE_MODE;
+
+    init();
 
     pthread_mutex_init(&camera_mutex, NULL);
 
